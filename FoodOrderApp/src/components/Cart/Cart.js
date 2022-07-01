95d9a7f2a9a5bd93a7b86135cde06c937a02037d
bonevboyan <boyanbonev43@gmail.com>
2022-07-02 00:12:01 +0300

    Finished Orders App

--- FoodOrderApp/src/components/Cart/Cart.js
+++ FoodOrderApp/src/components/Cart/Cart.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import CartContext from "../../store/cart-context";
 
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
@@ -10,13 +11,26 @@ const Cart = (props) => {
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
     const hasItems = cartCtx.items.length > 0;
 
+    const cartItemRemoveHandler = (id) => {
+        cartCtx.removeItem(id);
+    };
+
+    const cartItemAddHandler = (item) => {
+        cartCtx.addItem({...item, amount: 1});
+    };
+
     const cartItems = (
-        <ul className={classes['cart-items']}>
-            {cartCtx.items.map(
-                (item) => (
-                    <li>{item.name}</li>
-                )
-            )}
+        <ul className={classes["cart-items"]}>
+            {cartCtx.items.map((item) => (
+                <CartItem
+                    key={item.id}
+                    name={item.name}
+                    amount={item.amount}
+                    price={item.price}
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                    onAdd={cartItemAddHandler.bind(null, item)}
+                />
+            ))}
         </ul>
     );
 
@@ -28,7 +42,12 @@ const Cart = (props) => {
                 <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
-                <button className={classes["button--alt"]} onClick={props.onCloseCart}>Close</button>
+                <button
+                    className={classes["button--alt"]}
+                    onClick={props.onCloseCart}
+                >
+                    Close
+                </button>
                 {hasItems && <button className={classes.button}>Order</button>}
             </div>
             <div></div>
