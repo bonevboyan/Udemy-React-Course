c05676c99cd781273ecbccd970071de11153c823
bonevboyan <boyanbonev43@gmail.com>
2022-07-01 19:49:54 +0300

    Worked on Food Order App

--- FoodOrderApp/src/components/Layout/Header.js
+++ FoodOrderApp/src/components/Layout/Header.js
@@ -2,18 +2,20 @@ import React from "react";
 
 import HeaderCardButton from "./HeaderCartButton";
 import mealsImage from "../../assets/meals.jpg";
-import classes from "./Header.module.css"
+import classes from "./Header.module.css";
 
 const Header = (props) => {
-    return <React.Fragment>
-        <header className={classes.header}>
-            <h1>ReactMeals</h1>
-            <HeaderCardButton />
-        </header>
-        <div className={classes['main-image']}>
-            <img src={mealsImage} alt="A table full of food!"/>
-        </div>
-    </React.Fragment>
-}
+    return (
+        <React.Fragment>
+            <header className={classes.header}>
+                <h1>ReactMeals</h1>
+                <HeaderCardButton onClick={props.onShowCart} />
+            </header>
+            <div className={classes["main-image"]}>
+                <img src={mealsImage} alt="A table full of food!" />
+            </div>
+        </React.Fragment>
+    );
+};
 
 export default Header;
--- FoodOrderApp/src/components/Layout/HeaderCartButton.js
+++ FoodOrderApp/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,22 @@
+import { useContext } from "react";
+
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css"
 
 const HeaderCardButton = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => curNumber + item.amount, 0);
+
     return (
-        <button className={classes.button}>
+        <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
             <span className={classes.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     );
