cd9379b67c096c724d0b2ec57d2f27028dcdb386
bonevboyan <boyanbonev43@gmail.com>
2022-07-17 16:31:05 +0300

    Finished NextJS Practice

--- NextJsPracticeProject/pages/[meetupId]/index.js
+++ NextJsPracticeProject/pages/[meetupId]/index.js
@@ -1,52 +1,75 @@
-import MeetupDetail from "../../components/meetups/MeetupDetail";
+import { MongoClient, ObjectId } from 'mongodb';
+import { Fragment } from 'react';
+import Head from 'next/head';
 
-function MeetupDetails() {
-    return (
-        <MeetupDetail
-            image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
-            title="First Meetup"
-            address="Some Street 5, Some City"
-            description="This is a first meetup"
-        />
-    );
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails(props) {
+  return (
+    <Fragment>
+      <Head>
+        <title>{props.meetupData.title}</title>
+        <meta name='description' content={props.meetupData.description} />
+      </Head>
+      <MeetupDetail
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
+      />
+    </Fragment>
+  );
 }
 
 export async function getStaticPaths() {
-    return {
-        fallback: false,
-        paths: [
-            {
-                params: {
-                    meetupId: "m1",
-                },
-            },
-            {
-                params: {
-                    meetupId: "m2",
-                },
-            },
-        ],
-    };
+  const client = await MongoClient.connect(
+    'mongodb+srv://admin:admin@cluster0.b7aj7uc.mongodb.net/?retryWrites=true&w=majority'
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
+  return {
+    fallback: 'blocking',
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
+  };
 }
 
 export async function getStaticProps(context) {
-    // fetch data for a single meetup
+  // fetch data for a single meetup
 
-    const meetupId = context.params.meetupId;
+  const meetupId = context.params.meetupId;
 
-    console.log(meetupId);
+  const client = await MongoClient.connect(
+    'mongodb+srv://admin:admin@cluster0.b7aj7uc.mongodb.net/?retryWrites=true&w=majority'
+  );
+  const db = client.db();
 
-    return {
-        props: {
-            meetupData: {
-                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
-                id: meetupId,
-                title: "First Meetup",
-                address: "Some Street 5, Some City",
-                description: "This is a first meetup",
-            },
-        },
-    };
+  const meetupsCollection = db.collection('meetups');
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
+
+  client.close();
+
+  return {
+    props: {
+      meetupData: {
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
--- NextJsPracticeProject/pages/index.js
+++ NextJsPracticeProject/pages/index.js
@@ -1,24 +1,22 @@
-import MeetupList from "../components/meetups/MeetupList";
+import { Fragment } from 'react';
+import Head from 'next/head';
+import { MongoClient } from 'mongodb';
 
-const DUMMY_MEETUPS = [
-    {
-        id: "m1",
-        title: "A First Meetup",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
-        address: "Some address 5, 12345 Some City",
-        description: "This is a first meetup!",
-    },
-    {
-        id: "m2",
-        title: "A Second Meetup",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
-        address: "Some address 10, 12345 Some City",
-        description: "This is a second meetup!",
-    },
-];
+import MeetupList from '../components/meetups/MeetupList';
 
 function HomePage(props) {
-    return <MeetupList meetups={props.meetups} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name='description'
+          content='Browse a huge list of highly active React meetups!'
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />;
+    </Fragment>
+  );
 }
 
 // export async function getServerSideProps(context) {
@@ -35,13 +33,29 @@ function HomePage(props) {
 // }
 
 export async function getStaticProps() {
-    // fetch data from an API
-    return {
-        props: {
-            meetups: DUMMY_MEETUPS,
-        },
-        revalidate: 1,
-    };
+  // fetch data from an API
+  const client = await MongoClient.connect(
+    'mongodb+srv://admin:admin@cluster0.b7aj7uc.mongodb.net/?retryWrites=true&w=majority'
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
 }
 
 export default HomePage;
